e433517ddc09ddf61caf5e103f448fe089758c20
Andrii Pyrozhok <pyrozhok.dev@gmail.com>
2023-12-05 08:54:26 -0700

    Background generator random function

--- Complete Web Developer/Document object model/Background generator/script.js
+++ Complete Web Developer/Document object model/Background generator/script.js
@@ -13,6 +13,28 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function randomize() {
+	color1.value = randomColors();
+	color2.value = randomColors();
+	body.style.background = "linear-gradient(to right, "
+		+ color1.value
+		+ ", "
+		+ color2.value
+		+ ")";
+
+	css.textContent = body.style.background + ";";
+
+	document.getElementById('p1').style.color = randomColors();
+}
+
+
+// random colors - taken from here:
+// http://www.paulirish.com/2009/random-hex-color-code-snippets/
+
+function randomColors() {
+	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
