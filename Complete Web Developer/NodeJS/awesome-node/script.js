d88533ef3f617c219ad6d0640c9e5dc4bc37c71c
Andrii Pyrozhok <pyrozhok.dev@gmail.com>
2024-08-23 07:35:14 -0600

    Append, write, delete

--- Complete Web Developer/NodeJS/awesome-node/script.js
+++ Complete Web Developer/NodeJS/awesome-node/script.js
@@ -7,8 +7,31 @@ fs.readFile('./hello.txt', (err, data) => {
 	if (err) {
 		console.log('errrroooorrr');
 	}
-	console.log('1', data.toString());
+	console.log('Async', data.toString());
 })
 
 const file = fs.readFileSync('./hello.txt');
-console.log('2', file.toString());
+console.log('Sync', file.toString());
+
+// // APPEND (add line to txt file)
+// fs.appendFile('./hello.txt', ' This is so cool!', err => {
+// 	if (err) {
+// 		console.log(err)
+// 	}
+// })
+
+// // WRITE
+// fs.writeFile('bye.txt', 'Sad to see you go', err => {
+// 	if (err) {
+// 		console.log(err)
+// 	}
+// })
+
+// DELETE
+fs.unlink('./bye.txt', err => {
+	if (err) {
+		console.log(err)
+	}
+	console.log('Inception')
+})
+
